0c4a039de5e213c31cb595a15e4abe7ec9ac0634
AngieCana <grilozangie@gmail.com>
2023-06-01 15:03:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//let x=15;
+//let y=30;
+
+
+//console.log(x);
+
+//let names =["jim", "james", "paul", "tyrone"];
+//console.log(names);
+
+let seed1 = "carrots";
+console.log(seed1);
+
+let vegetables = 20;
+console.log(vegetables);
+
+let x= 12;
+let a =78;
+
+//total of x+a
+
+if (x === 12 && a ===78)
+  { 
+    let sum = x+a;
+    console.log(sum);
+  }
+
