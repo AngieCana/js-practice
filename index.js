888a07de293361cacbcaf7d4512d4ca0a7726e8f
AngieCana <grilozangie@gmail.com>
2023-06-02 14:46:30 -0400

    first commit

--- index.js
+++ index.js
@@ -7,20 +7,99 @@
 //let names =["jim", "james", "paul", "tyrone"];
 //console.log(names);
 
-let seed1 = "carrots";
-console.log(seed1);
+// let seed1 = "carrots";
+// console.log(seed1);
 
-let vegetables = 20;
-console.log(vegetables);
+// let vegetables = 20;
+// console.log(vegetables);
 
-let x= 12;
-let a =78;
+// let x= 12;
+// let a =78;
+// let age = 18;
 
-//total of x+a
+// //total of x+a
 
-if (x === 12 && a ===78)
-  { 
-    let sum = x+a;
-    console.log(sum);
-  }
+// if (x === 12 && a ===78)
+//   { 
+//     let sum = x+a;
+//     console.log(sum);
+//   }
+  
+//   if (age >= 18){
+//     //do this
+//     console.log("access granted");
+//   }
+// else{
+//     //do this
+//     console.log("access denied");
+// }
+
+// let wood = 19;
+
+// if (wood > 40)
+// {
+//   console.log("You have enough for a treehouse.");
+// }
+// else if (wood >= 20)
+// {
+//   console.log("You can build a stool.");
+// }
+// else{
+//     console.log("You are S.O.L.");
+// }
+
+// let num = 101;
+
+// if(num > 0 && num > 100)
+// {
+//     console.log("Your number is positive and greater than 100.");
+// }
+// else if (num > 0 && num < 100)
+// {
+//     console.log("Your number is positive but less than 100.");
+// }
+// else if (num < 0)
+// {
+//     console.log("Your number is negative.");
+// }
+// else if(num === 100)
+// {
+//     console.log("Your number is 100.")
+// }
+// else {
+// console.log("Your number is 0.");
+// }
+
+
+// let username = "admin";
+// let password = "Pass614";
+
+// if (username === "admin" && password === "Pass614"){
+//     console.log("Access Granted")
+// }
+// else{
+//     console.log("Access Denied");
+// }
+
+let score = 79;
+
+if (score >= 80)
+    {
+        console.log("You did a good job");
+    }
+    else if (score >= 70 && score < 80)
+    {
+        console.log("Keep Trying");
+    }
+    else if(score >= 60 && score < 70)
+    {
+        console.log("ehhhhh");
+    }
+    else if(score >= 55 && score < 60)
+    {
+        console.log("Not Too Good");
+    }
+    else{
+        console.log("Bad Grade But You'll get Em Next time");
+    }
 
